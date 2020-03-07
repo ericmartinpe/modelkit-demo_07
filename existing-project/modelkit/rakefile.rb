54b8dd3d5633dca29957f181115bdf32e79b0255
ericwmartin <ericmartinpe@gmail.com>
2020-03-07 15:36:04 -0500

    Modify rakefile :compose and :sim and remove :cases.

--- existing-project/modelkit/rakefile.rb
+++ existing-project/modelkit/rakefile.rb
@@ -1,31 +1,20 @@
 ep_dir = "C:/EnergyPlusV8-9-0"
 modelkit_dir = "~/Documents/Modelkit Catalyst/"
 template_dir = modelkit_dir + "templates/net-zero-templates/templates"
-weather_dir = template_dir + "/weather/"
 main_dir = File.expand_path(File.dirname(__FILE__)) # main_dir is the directory of this rakefile
+weather_dir = main_dir + "/templates/weather/"
 runs_dir = main_dir + "/runs/"
 cases_dir = main_dir + "/parameters/cases/"
 
-locations = ['CZ01','CZ02','CZ03','CZ04','CZ05','CZ06','CZ07','CZ08','CZ09','CZ10','CZ11','CZ12','CZ13','CZ14','CZ15','CZ16']
-locations =['CZ01']
-
-desc "Generate case files"
-task :cases do
-  for location in locations
-    puts "  Generating case files for: #{location}"
-    pxv_path = cases_dir + "#{location}.pxv"
-    pxv_string = ""
-    pxv_string << ":location=>\"#{location}\""
-    File.write(pxv_path, pxv_string)
-  end
-end
+locations = ['USA_CO_Aurora-Buckley.Field.ANGB.724695_TMY3']
+runs =['base','prop']
 
 desc "Compose models from case files"
 task :compose do
-  for location in locations
-    pxv_path = cases_dir + "#{location}.pxv"
-    puts "  Composing Case: #{location}"
-    output_dir = runs_dir + "#{location}/"
+  for run in runs
+    pxv_path = cases_dir + "#{run}.pxv"
+    puts "  Composing Case: #{run}"
+    output_dir = runs_dir + "#{run}/"
     output_path = output_dir + "instance.idf"
     root_dir = main_dir + "/templates/"
     root_path = root_dir + "root.pxt"
@@ -35,10 +24,11 @@ end
 
 desc "Simulate models in the runs directory"
 task :sim do
-  for location in locations
-    output_dir = runs_dir + "#{location}/"
+  for run in runs
+    output_dir = runs_dir + "#{run}/"
     output_path = output_dir + "instance.idf"
-    weather_path = weather_dir + "#{location}.epw"
+    weather_path = weather_dir + "#{locations[0]}.epw"
+    puts weather_path
     system "modelkit-energyplus energyplus-run --weather=\"#{weather_path}\" \"#{output_path}\""
   end
 end
